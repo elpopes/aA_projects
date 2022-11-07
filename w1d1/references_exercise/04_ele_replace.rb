36ebc42e3ef17d795e12a9f82a47615a2d862019
lorenzo <lorenzotijerina@gmail.com>
2022-11-07 14:26:45 -0500

    ref exercises

--- w1d1/references_exercise/04_ele_replace.rb
+++ w1d1/references_exercise/04_ele_replace.rb
@@ -7,7 +7,12 @@
 # to be the same before and after calling your method.
 
 def ele_replace!(array, hash)
-
+    array.each_with_index do |ele, i|
+        if hash.has_key?(ele) 
+            array[i] = hash[ele]
+        end
+    end
+    array
 end
 
 
