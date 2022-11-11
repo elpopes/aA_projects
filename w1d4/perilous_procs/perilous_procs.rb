02f9f358395a55e77619722ddcecfd17f4e14b53
lorenzo <lorenzotijerina@gmail.com>
2022-11-10 22:44:03 -0500

    perilous procs

--- w1d4/perilous_procs/perilous_procs.rb
+++ w1d4/perilous_procs/perilous_procs.rb
@@ -49,3 +49,341 @@ def at_least?(array, n, &blc)
     array.each {|ele| counter += 1 if blc.call(ele)}
     counter >= n
 end
+
+puts "at_least?"
+p at_least?(['sad', 'quick', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
+# false
+p at_least?(['sad', 'quickly', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
+# false
+p at_least?(['sad', 'quickly', 'timidly', 'final'], 2) { |s| s.end_with?('ly') }
+# true
+p at_least?(['sad', 'quickly', 'timidly', 'finally'], 2) { |s| s.end_with?('ly') }
+# true
+p at_least?(['sad', 'quickly', 'timid', 'final'], 1) { |s| s.end_with?('ly') }
+# true
+p at_least?(['sad', 'quick', 'timid', 'final'], 1) { |s| s.end_with?('ly') }
+# false
+p at_least?([false, false, false], 3) { |bool| bool }
+# false
+p at_least?([false, true, true], 3) { |bool| bool }
+# false
+p at_least?([true, true, true], 3) { |bool| bool }
+# true
+p at_least?([true, true, true, true], 3) { |bool| bool }
+# true
+puts
+
+def every?(array, &blc)
+    array.each {|ele| return false unless blc.call(ele)}
+    return true
+end
+
+puts "every?"
+p every?([3, 1, 11, 5]) { |n| n.even? }                                 # false
+p every?([2, 4, 4, 8]) { |n| n.even? }                                  # true
+p every?([8, 2]) { |n| n.even? }                                        # true
+p every?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' }  # false
+p every?(['squash', 'pea', 'kale', 'potato']) { |str| str[0] == 'p' }   # false
+p every?(['parsnip', 'potato', 'pea']) { |str| str[0] == 'p' }          # true
+puts
+
+def at_most?(array, n, &blc)
+    counter = 0
+    array.each {|ele| counter += 1 if blc.call(ele)}
+    counter <= n
+end
+
+puts "at_most?"
+p at_most?([-4, 100, -3], 1) { |el| el > 0 }                         # true
+p at_most?([-4, -100, -3], 1) { |el| el > 0 }                        # true
+p at_most?([4, 100, -3], 1) { |el| el > 0 }                          # false
+p at_most?([4, 100, 3], 1) { |el| el > 0 }                           # false
+p at_most?(['r', 'q', 'e', 'z'], 2) { |el| 'aeiou'.include?(el) }    # true
+p at_most?(['r', 'i', 'e', 'z'], 2) { |el| 'aeiou'.include?(el) }    # true
+p at_most?(['r', 'i', 'e', 'o'], 2) { |el| 'aeiou'.include?(el) }    # false
+puts
+
+def first_index(array, &blc)
+    array.each_with_index {|ele, i| return i if blc.call(ele)}
+    return nil
+end
+
+puts "first_index"
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 }           # 2
+p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } # 0
+p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } # nil
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }            # nil
+puts
+
+def xnor_select(array, prc_1, prc_2)
+    array.select {|ele| prc_1.call(ele) && prc_2.call(ele) || !prc_1.call(ele) && !prc_2.call(ele) }
+end
+
+puts "xnor_select"
+is_even = Proc.new { |n| n % 2 == 0 }
+is_odd = Proc.new { |n| n % 2 != 0 }
+is_positive = Proc.new { |n| n > 0 }
+p xnor_select([8, 3, -4, -5], is_even, is_positive)         # [8, -5]
+p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive)  # [-7, -13, 12]
+p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)   # [5, -10]
+puts
+
+
+def filter_out!(array, &blc)
+
+    array.select! {|ele| ele unless blc.call(ele)}
+
+end
+
+puts "filter_out!"
+arr_1 = [10, 6, 3, 2, 5 ]
+filter_out!(arr_1) { |x| x.odd? }
+p arr_1     # [10, 6, 2]
+
+arr_2 = [1, 7, 3, 5 ]
+filter_out!(arr_2) { |x| x.odd? }
+p arr_2     # []
+
+arr_3 = [10, 6, 3, 2, 5 ]
+filter_out!(arr_3) { |x| x.even? }
+p arr_3     # [3, 5]
+
+arr_4 = [1, 7, 3, 5 ]
+filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
+p arr_4 # [1, 7, 3, 5]
+puts
+
+
+def multi_map(array, n=1, &blc)
+    n.times do 
+        array.map! {|ele| blc.call(ele)}
+    end
+    array
+end
+
+puts "multimap"
+p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
+p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'}   # ["pretty!", "cool!", "huh?!"]
+p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'}   # ["pretty!!!", "cool!!!", "huh?!!!"]
+p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
+p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
+p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             # [40000, 30000, 20000, 70000]
+puts 
+
+
+def proctition(array, &blc)
+    array.partition {|ele| blc.call(ele)}.flatten
+end
+
+puts "proctition"
+p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 }
+# [4, 7, 1, 3, -5, -10, -2]
+
+p proctition([7, 8, 3, 6, 10]) { |el| el.even? }
+# [8, 6, 10, 7, 3]
+
+p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
+# ["boot", "bug", "boat", "cat", "dog"]
+puts
+
+
+def selected_map!(array, prc_1, prc_2)
+    array.map! {|ele| prc_1.call(ele) ? prc_2.call(ele) : ele}
+    return nil
+end
+
+puts "selected_map!"
+is_even = Proc.new { |n| n.even? }
+is_positive = Proc.new { |n| n > 0 }
+square = Proc.new { |n| n * n }
+flip_sign = Proc.new { |n| -n }
+
+arr_1 = [8, 5, 10, 4]
+p selected_map!(arr_1, is_even, square)     # nil
+p arr_1                                     # [64, 5, 100, 16]
+
+arr_2 = [-10, 4, 7, 6, -2, -9]
+p selected_map!(arr_2, is_even, flip_sign)  # nil
+p arr_2                                     # [10, -4, 7, -6, 2, -9]
+
+arr_3 = [-10, 4, 7, 6, -2, -9]
+p selected_map!(arr_3, is_positive, square) # nil
+p arr_3                                     # [-10, 16, 49, 36, -2, -9]
+puts
+
+
+def chain_map(val, prcs_array)
+    prcs_array.inject(val){|acc, prc| prc.call(acc)}
+end
+
+add_5 = Proc.new { |n| n + 5 }
+half = Proc.new { |n| n / 2.0 }
+square = Proc.new { |n| n * n }
+
+p chain_map(25, [add_5, half])          # 15.0
+p chain_map(25, [half, add_5])          # 17.5
+p chain_map(25, [add_5, half, square])  # 225
+p chain_map(4, [square, half])          # 8
+p chain_map(4, [half, square])          # 4
+puts
+
+def proc_suffix(sentence, hash)
+    words = sentence.split(" ")
+    words.each_with_index do |word, i| 
+        hash.each {|prc, sfx| words[i] += sfx if prc.call(word)}
+    end
+    words.join(" ")
+end
+
+puts "proc_suffix"
+contains_a = Proc.new { |w| w.include?('a') }
+three_letters = Proc.new { |w| w.length == 3 }
+four_letters = Proc.new { |w| w.length == 4 }
+
+p proc_suffix('dog cat',
+    contains_a => 'ly',
+    three_letters => 'o'
+)   # "dogo catlyo"
+
+p proc_suffix('dog cat',
+    three_letters => 'o',
+    contains_a => 'ly'
+)   # "dogo catoly"
+
+p proc_suffix('wrong glad cat',
+    contains_a => 'ly',
+    three_letters => 'o',
+    four_letters => 'ing'
+)   # "wrong gladlying catlyo"
+
+p proc_suffix('food glad rant dog cat',
+    four_letters => 'ing',
+    contains_a => 'ly',
+    three_letters => 'o'
+)   # "fooding gladingly rantingly dogo catlyo"
+puts
+
+
+require "byebug"
+def proctition_platinum(array, *prcs)
+    hash = Hash.new{|h,k| h[k] = []}
+    (1..prcs.length).each {|i| hash[i]}
+    prcs.each_with_index do |prc, i| 
+        array.each do |ele| 
+            if prc.call(ele) && !hash.values.flatten.include?(ele) ## values are arrays not elements
+                # debugger
+                hash[i+1] << ele 
+            end
+        end
+    end
+    hash
+end
+
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+begins_w = Proc.new { |s| s.downcase[0] == 'w' }
+
+puts "proctition_platinum"
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, contains_a)
+# {1=>["when!", "WHERE!"], 2=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, is_upcase, contains_a)
+# {1=>["when!", "WHERE!"], 2=>["WHO", "WHY"], 3=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_upcase, is_yelled, contains_a)
+# {1=>["WHO", "WHERE!", "WHY"], 2=>["when!"], 3=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], begins_w, is_upcase, is_yelled, contains_a)
+# {1=>["WHO", "what", "when!", "WHERE!", "WHY"], 2=>[], 3=>[], 4=>[]}
+puts
+
+
+def procipher(sentence, hash)
+    words = sentence.split(" ")
+    words.each_with_index {|word, i| hash.each {|k,v| words[i] = v.call(words[i]) if k.call(word)}}
+    words.join(" ")
+end
+
+puts "procipher"
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+make_question = Proc.new { |s| s + '???' }
+reverse = Proc.new { |s| s.reverse }
+add_smile = Proc.new { |s| s + ':)' }
+
+p procipher('he said what!',
+    is_yelled => make_question,
+    contains_a => reverse
+) # "he dias ???!tahw"
+
+p procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => make_question
+) # "he dias !tahw???"
+
+p procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => add_smile
+) # "he dias !tahw:)"
+
+p procipher('stop that taxi now',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "stop that??? taxi??? now"
+
+p procipher('STOP that taxi now!',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "STOP:) that??? taxi??? !won"
+puts
+
+
+
+def picky_procipher(sentence, hash)
+    words = sentence.split(" ")
+    words.each_with_index {|word, i| hash.reverse_each {|k,v| words[i] = v.call(word) if k.call(word)}}
+    words.join(" ")
+end
+
+puts "picky_procipher"
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+make_question = Proc.new { |s| s + '???' }
+reverse = Proc.new { |s| s.reverse }
+add_smile = Proc.new { |s| s + ':)' }
+
+p picky_procipher('he said what!',
+    is_yelled => make_question,
+    contains_a => reverse
+) # "he dias what!???"
+
+p picky_procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => make_question
+) # "he dias !tahw"
+
+p picky_procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => add_smile
+) # "he dias !tahw"
+
+p picky_procipher('stop that taxi now',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "stop that??? taxi??? now"
+
+p picky_procipher('STOP that taxi!',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "STOP:) that??? !ixat"
